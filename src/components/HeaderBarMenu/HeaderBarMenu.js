981f4afa20dee1e6254a1354a01af3df3e10447a
Daniel <learn.logvin@gmail.com>
2019-11-10 07:46:02 +0200

    gggg

---
+++ src/components/HeaderBarMenu/HeaderBarMenu.js
@@ -0,0 +1,13 @@
+import React from "react";
+
+class HeaderBarMenu extends React.Component{
+    render(){
+        return(
+            <div style={{backgroundColor: "black",flex:0.2}}>
+
+            </div>
+        );
+    }
+}
+
+export default HeaderBarMenu;
